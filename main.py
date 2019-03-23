d9582b7432cf3e17635d6dd887394bc459453f0f
Muhammed Cavusoglu <mcavus495@gmail.com>
2019-03-23 23:55:05 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Age Estimation using TensorFlow
+# Muhammed Cavusoglu (21400653) and Kemal Buyukkaya (21200496)
+
+import numpy as np
+import tensorflow as tf
+
+def cnn_model():
+    print "model"
+    
+def main():
+    print "main"
+    
+if __name__ == "__main__":
+  # some run fn. like tf.app.run()
+  main()
